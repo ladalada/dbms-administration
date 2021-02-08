39058de9744faa4a06ff10147f6f410b1b911def
Lada Astashenkova <lada.astashenkova@yandex.ua>
2021-02-08 18:53:32 +0300

    Update 2_restore.sql

--- 2_restore.sql
+++ 2_restore.sql
@@ -1,9 +1,9 @@
 -- DROP PROCEDURE IF EXISTS restore_backup;
 
 /*
-Хранимая процедура для восстановления бд
-из хранимого набора резервных копий (бэкапов)
-с указанием даты и времени
+Stored procedure for restoring a database
+from a stored set of backup copies (backups)
+with date and time
 */
 
 CREATE PROCEDURE restore_backup 
@@ -11,7 +11,7 @@ CREATE PROCEDURE restore_backup
 	@restore_datetime NVARCHAR(256) 
 AS
 BEGIN
-	-- Проверка, есть ли бэкапы у бд
+	-- Checking if the database has backups
 	IF EXISTS (SELECT * FROM master.dbo.table_backups b WHERE b.backup_db = @db)
 	BEGIN
 		
@@ -20,7 +20,7 @@ BEGIN
 		DECLARE @full_path NVARCHAR(256);
 		DECLARE @diff_path NVARCHAR(256);
 		
-		-- Поиск последнего полного бэкапа с датой создания <= @restore_datetime
+		-- Find the last full backup with creation date <= @restore_datetime
 		SELECT @full_datetime = (
 			SELECT TOP 1 backup_datetime
 			FROM master.dbo.table_backups
@@ -28,7 +28,7 @@ BEGIN
 				AND backup_datetime <= @restore_datetime
 			ORDER BY backup_datetime DESC);
 
-		-- Поиск пути для найденного полного бэкапа
+		-- Finding the path for the found full backup
 		SELECT @full_path = (
 			SELECT TOP 1 backup_path
 			FROM master.dbo.table_backups
@@ -36,7 +36,7 @@ BEGIN
 				AND backup_datetime = @full_datetime
 			ORDER BY backup_datetime DESC);
 
-		-- Поиск последнего разностного бэкапа с датой создания <= @restore_datetime
+		-- Find the last differential backup with creation date <= @restore_datetime
 		SELECT @diff_datetime = (
 			SELECT TOP 1 backup_datetime 
 			FROM master.dbo.table_backups
@@ -44,7 +44,7 @@ BEGIN
 				AND backup_datetime <= @restore_datetime
 			ORDER BY backup_datetime DESC);
 
-		-- Поиск пути для найденного разностного бэкапа
+		-- Finding the path for the found differential backup
 		SELECT @diff_path = (
 			SELECT TOP 1 backup_path
 			FROM master.dbo.table_backups
@@ -52,24 +52,24 @@ BEGIN
 				AND backup_datetime = @diff_datetime
 			ORDER BY backup_datetime DESC);
 
-		-- Если полный бэкап создан до разностного
+		-- If a full backup was created before the differential
 		IF @full_datetime < @diff_datetime 
 		BEGIN
-			-- восстановление полного бэкапа
+			-- restore full backup
 			RESTORE DATABASE @db
 			FROM DISK = @full_path
 			WITH REPLACE, NORECOVERY;
 
-			-- восстановление разностного бэкапа
+			-- restoring differential backup
 			RESTORE DATABASE @db
 			FROM DISK = @diff_path
 			WITH RECOVERY;
 		END
 
-		-- Если разностный бэкап создан до полного
+		-- If a differential backup was created before a full
 		ELSE
 		BEGIN
-			-- восстановление только полного бэкапа
+			-- restoring only a full backup
 			RESTORE DATABASE @db
 			FROM DISK = @full_path
 			WITh REPLACE, RECOVERY;
