e9d8a2cab668258cfe9d9cca7a7a7934f3c9c3a0
Lada Astashenkova <lada.astashenkova@yandex.ua>
2021-02-08 18:46:38 +0300

    Update 2_backup_procedure.sql

--- 2_backup_procedure.sql
+++ 2_backup_procedure.sql
@@ -1,12 +1,12 @@
 -- USE master;
 
 /*
-Хранимая процедура для создания резервной копии (бэкапа) заданной бд.
-	Если бэкап бд создается впервые 
-	или если с даты создания последнего полного бэкапа прошло больше месяца,
-		то создается полный бэкап.
-	Иначе 
-		создается разностный бэкап. 
+Stored procedure for creating a backup copy (backup) of the specified database.
+	If a database backup is created for the first time, 
+	or if more than a month has passed since the last full backup was created,
+		a full backup is created.
+	Otherwise, 
+		a differential backup is created. 
 */
 
 CREATE PROCEDURE create_backup @db NVARCHAR(256) AS
@@ -18,7 +18,7 @@ BEGIN
 	SET @datetime = CAST(SYSDATETIME() AS NVARCHAR(256)); -- 2021-01-21 00:00:00.7906311
 	SET @datetime_name = (SELECT REPLACE(REPLACE(@datetime, ' ', 'T'), ':', '.')); -- 2021-01-21T00.00.00.7906311
 
-	-- Бэкап создается впервые, поэтому создается полный бэкап
+	-- The backup is created for the first time, so a full backup is created
 	IF NOT EXISTS (SELECT * FROM master.dbo.table_backups b WHERE b.backup_db = @db)
 	BEGIN
 		SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER03\MSSQL\DATA\' + @db + '_full_' + @datetime_name + '.bac';
@@ -28,10 +28,10 @@ BEGIN
 		INSERT INTO master.dbo.table_backups VALUES (@db, @datetime, @path, 1); -- backup_is_full = 1
 	END
 
-	-- Бэкап создается не впервые
+	-- This is not the first time a backup is created
 	ELSE
 	BEGIN
-		-- Самая поздняя дата создания полного бэкапа
+		-- Latest date of creation of full backup
 		DECLARE @last_full_datetime NVARCHAR(256);
 		SELECT @last_full_datetime = t.backup_datetime FROM (
 			SELECT TOP 1 backup_datetime
@@ -41,7 +41,7 @@ BEGIN
 			ORDER BY backup_datetime DESC
 		) t;
 		
-		-- С даты создания последнего полного бэкапа прошло больше месяца, поэтому создается полный бэкап
+		-- More than a month has passed since the last full backup was created, so a full backup is being created
 		IF DATEDIFF(month, @last_full_datetime, @datetime) > 1
 		BEGIN
 			SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER03\MSSQL\DATA\' + @db + '_full_' + @datetime_name + '.bac';
@@ -51,7 +51,7 @@ BEGIN
 			INSERT INTO master.dbo.table_backups VALUES (@db, @datetime, @path, 1); -- backup_is_full = 1
 		END
 
-		-- Создание разностного бэкапа
+		-- Creating a differential backup
 		ELSE
 		BEGIN
 			SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER03\MSSQL\DATA\' + @db + '_diff_' + @datetime_name + '.bac';
